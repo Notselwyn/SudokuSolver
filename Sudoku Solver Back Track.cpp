c3a277237f352437596cbd60679122db04208a3a
Lau <68616630+Notselwyn@users.noreply.github.com>
2021-06-03 20:16:24 +0200

    Add files via upload

--- Sudoku Solver Back Track.cpp
+++ Sudoku Solver Back Track.cpp
@@ -1,39 +1,44 @@
 #include <iostream>
 #include <chrono>
+
 using namespace std::chrono;
-
-
 using namespace std;
 
 int main() {
-    int first_array[9][9] = {{5, 3, 0, 0, 7, 0, 0, 0, 0},
-                             {6, 0, 0, 1, 9, 5, 0, 0, 0},
-                             {0, 9, 8, 0, 0, 0, 0, 6, 0},
-                             {8, 0, 0, 0, 6, 0, 0, 0, 3},
-                             {4, 0, 0, 8, 0, 3, 0, 0, 1},
-                             {7, 0, 0, 0, 2, 0, 0, 0, 6},
-                             {0, 6, 0, 0, 0, 0, 2, 8, 0},
-                             {0, 0, 0, 4, 1, 9, 0, 0, 5},
-                             {0, 0, 0, 0, 8, 0, 0, 7, 0}
+    // Enter the sudoku here!!!
+    // The numbers are the cells in the sudoku
+    int first_array[9][9] = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             {0, 0, 0, 0, 0, 0, 0, 0, 0}
     };
     int first_array_flat[81];
     int new_array[9][9];
     int new_array_flat[81];
+    int threebythree_box[3][3];
+    int crosshair_cells[25];
+    int ra[81];
     int num_index = 0;
     bool plus_one = false;
     for (int i=0; i<9; i++) {
         for (int j=0; j<9; j++) {
             first_array_flat[i*9+j] = first_array[i][j];
             new_array_flat[i*9+j] = first_array[i][j];
+            ra[i*9+j] = first_array[i][j];
             new_array[i][j] = first_array[i][j];
         }
     }
-
+    for (int low=0, high=80; low < high; low++, high--){
+        swap(ra[low], ra[high]);
+    }
+    int end_index = 80-*find(begin(ra), end(ra), 0);
     auto start = high_resolution_clock::now();
     while (num_index < 81) {
-        int new_array[9][9];
-        int threebythree_box[3][3];
-        int crosshair_cells[25];
         for (int i=0; i<81; i++) {
             new_array[i/9][i%9] = new_array_flat[i];
         }
@@ -51,27 +56,29 @@ int main() {
             if (i != num_index / 9) {
                 crosshair_cells[i+9] = new_array[i][num_index % 9];
             }
-            if (i != ((num_index % 9) / 9) * 3 + (num_index % 3)) {
+            if (i != num_index % 3 + num_index / 9 % 3 * 3) {
                 crosshair_cells[i+16] = threebythree_box[i/3][i%3];
             }
         }
-
-        if (num_index == 80) {
+        if (num_index == end_index) {
             for (int i=1; i<10; i++) {
-                if (find(begin(crosshair_cells), end(crosshair_cells), i) != end(crosshair_cells)) {
+                if (find(begin(crosshair_cells), end(crosshair_cells), i) == end(crosshair_cells)) {
+                    new_array[end_index/9][end_index%9] = i;
                     break;
                 }
             }
-            new_array[num_index / 9][num_index % 9];
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(stop - start);
+            cout << "Finished - " << duration.count()/1000 << "ms" << endl;
             for (int i=0; i<9; i++) {
                 if (i%3==0 && i!=0) {
-                    cout << "------|-------|-------" << endl;
+                    cout << "-------|-------|-------" << endl;
                 }
                 for (int j=0; j<9; j++) {
                     if (j%3==0 && j!=0) {
-                        cout << "| ";
+                        cout << " |";
                     }
-                    cout << new_array[i][j] << " ";
+                    cout << " " << new_array[i][j];
                 }
                 cout << endl;
             }
@@ -108,8 +115,5 @@ int main() {
         }
         num_index += 1;
     }
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    cout << "Finished - " << duration.count()/1000 << "ms" << endl;
     return 0;
 }
