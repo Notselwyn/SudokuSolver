4612775241898ef4ce346b6b6f92c0616de40f1e
Lau <68616630+Notselwyn@users.noreply.github.com>
2021-06-04 09:24:05 +0200

    Add files via upload

--- Sudoku Solver Back Track.py
+++ Sudoku Solver Back Track.py
@@ -1,20 +1,20 @@
 import numpy as np
-import time
+import datetime
 
 
 def main():
     # Enter sudoku here!!!
     # The numbers are the cells in the sudoku
     first_array = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 0]
     ]
 
     # Required variables to start running
@@ -25,21 +25,21 @@ def main():
     num_index = 0
     plus_one = False
 
-    time_now = time.time()
+    time_now = datetime.datetime.now()
 
     while num_index < 81:
         new_array = np.reshape(new_array_flat, (9, 9))
         threebythree_box = new_array[(num_index // 27) * 3:(num_index // 27) * 3 + 3, num_index % 9 // 3 * 3:num_index % 9 // 3 * 3 + 3].tolist()
         crosshair_cells = new_array[num_index // 9, :].tolist() + new_array[:, num_index % 9].tolist() + [x for y in threebythree_box for x in y]
 
-        if num_index == 80:
+        if num_index == 80-first_array_flat[::-1].index(0):
             new_array[num_index // 9][num_index % 9] = [x for x in range(1, 10) if x not in crosshair_cells][-1]
             new_array_flat[num_index] = [x for x in range(1, 10) if x not in crosshair_cells][-1]
 
         # Removing the active num from crosshairs: Horizontal, Vertical, 3x3 Box
         crosshair_cells.pop(num_index % 9)
         crosshair_cells.pop(num_index // 9 + 8)
-        crosshair_cells.pop(16 + ((num_index % 9) // 9) * 3 + (num_index % 3))
+        crosshair_cells.pop(16 + num_index % 3 + num_index // 9 % 3 * 3)
 
         # Check if the number isn't one of the pre-defined one from the original sudoku
         if first_array_flat[num_index] == 0:
@@ -68,8 +68,7 @@ def main():
             num_index -= 2
 
         num_index += 1
-
-    print("Finished - " + str(round(time.time() - time_now, 4)) + "s")
+    print(f"Finished - {int((datetime.datetime.now() - time_now).total_seconds()*1000)}ms")
     for array_pos, array in enumerate(new_array):
         result_str = "\x1b[48;2;255;255;255m"
         if (array_pos+1) % 3 == 0 and 7 >= array_pos >= 2:
